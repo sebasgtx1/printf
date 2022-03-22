ec0984bd01c5010b8f94d052ecd02a39414d24af
SebasGTX1 <sebas-0202@hotmail.es>
2022-03-22 13:46:17 -0500

    Print (null) if str is null

--- _print_string.c
+++ _print_string.c
@@ -6,13 +6,19 @@
  */
 int print_string(va_list list)
 {
-	int size = 0, j;
+	int size = 0, i = 0, j;
+	char *nulcase = "(null)";
 	char *str;
+
 
 	str = va_arg(list, char *);
 
 	if (!str)
+	{
+		for (i = 0; nulcase[i]; i++)
+			write(1, &nulcase[i], 1);
 		return (0);
+	}
 
 	for (j = 0; str[j]; j++, size++)
 		write(1, &str[j], 1);
