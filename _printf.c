78a56d5ae204db28e3aa971a892b9939fd6edd91
SebasGTX1 <sebas-0202@hotmail.es>
2022-03-22 15:08:06 -0500

    Checking NULL cases

--- _printf.c
+++ _printf.c
@@ -14,6 +14,9 @@ int _printf(const char *format, ...)
 
 	va_start(list, format);
 
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+		return (-1);
+
 	while (format && format[i])
 	{
 		if (!format[i])
