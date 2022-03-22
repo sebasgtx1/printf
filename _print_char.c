87beb3dde4f01e31040821c59557d81a052e4b44
SebastianBlandon <blandonodz@gmail.com>
2022-03-22 14:19:02 -0500

    Try pass the checker [UPDATE]

---
+++ _print_char.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_char - function prints a char
+ * @str: is the char in a pointer form
+ * Return: size
+ */
+
+int print_char(va_list str)
+{
+	char value;
+
+	value = va_arg(str, int);
+	if (!value)
+		return (0);
+
+	write(1, &value, 1);
+	return (1);
+}
